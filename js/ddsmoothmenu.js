59a447ff4b081e1c194421f25d1ce4f3c5e712e6
bleekk <bleekk@outlook.com>
2012-08-06 20:32:25 +0200

    Dropdownmenu arrow images
    Show/hide arrow images in drop down menu

--- js/ddsmoothmenu.js
+++ js/ddsmoothmenu.js
@@ -48,7 +48,8 @@ getajaxmenu:function($, setting){ //function to fetch external page containing t
 
 
 buildmenu:function($, setting){
-	var $cms_path =  setting.cms_path
+	var $cms_path = setting.cms_path
+	var $submenu_images = setting.submenu_images
 	var smoothmenu=ddsmoothmenu
 	var $mainmenu=$("#"+setting.mainmenuid+">ul") //reference main menu UL
 	$mainmenu.parent().get(0).className=setting.classname || "ddsmoothmenu"
@@ -68,11 +69,15 @@ buildmenu:function($, setting){
 		this._dimensions={w:this.offsetWidth, h:this.offsetHeight, subulw:$subul.outerWidth(), subulh:$subul.outerHeight()}
 		this.istopheader=$curobj.parents("ul").length==1? true : false //is top level header?
 		$subul.css({top:this.istopheader && setting.orientation!='v'? this._dimensions.h+"px" : 0})
-		$curobj.children("a:eq(0)").css(this.istopheader? {paddingRight: smoothmenu.arrowimages.down[2]} : {}).append( //add arrow images
+		
+		if($submenu_images == 'y'){
+		$curobj.children("a:eq(0)").css(this.istopheader? {paddingRight: smoothmenu.arrowimages.down[2]} : {}
+				).append( //add arrow images
 			'<img src="'+$cms_path + (this.istopheader && setting.orientation!='v'? smoothmenu.arrowimages.down[1] : smoothmenu.arrowimages.right[1])
 			+'" class="' + (this.istopheader && setting.orientation!='v'? smoothmenu.arrowimages.down[0] : smoothmenu.arrowimages.right[0])
 			+ '" style="border:0;" />'
 		)
+		}
 		if (smoothmenu.shadow.enable && !smoothmenu.css3support){ //if shadows enabled and browser doesn't support CSS3 box shadows
 			this._shadowoffset={x:(this.istopheader?$subul.offset().left+smoothmenu.shadow.offsetx : this._dimensions.w), y:(this.istopheader? $subul.offset().top+smoothmenu.shadow.offsety : $curobj.position().top)} //store this shadow's offsets
 			if (this.istopheader)
